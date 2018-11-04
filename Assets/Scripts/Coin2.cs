31b448cea51e492504e09113c7754d7e8d22a99a
Erik S <eriksava@hotmail.com>
2018-11-04 11:00:10 -0800

    game ready

---
+++ Assets/Scripts/Coin2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Coin2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player"){
+
+            GameManager.instance1.IncrementScore();
+            GameManager.instance1.SaveData();
+            Destroy(gameObject);
+
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using BayatGames.SaveGamePro;
+
+public class GameManager : MonoBehaviour {
+
+
+    public static GameManager instance1;
+
+    public Text scoreText;
+
+
+   
+
+    int score = 0;
+    int defaultScore = 0;
+
+
+    private void Awake()
+    {
+        instance1 = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+        Reload();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void IncrementScore(){
+
+
+            score++;
+            scoreText.text = score.ToString();
+
+    }
+
+    public void SaveData(){
+
+        SaveGame.Save<int>("score", score);
+    }
+
+    public void Reload(){
+
+        int lastScore = SaveGame.Load<int>("score", defaultScore);
+        score = lastScore;
+        scoreText.text = lastScore.ToString();
+    }
+
+}
